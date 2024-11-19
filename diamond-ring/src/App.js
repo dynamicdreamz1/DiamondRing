dfe796270d6fe30a9ca4a0e03ee201cef36a5710
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-11-19 17:06:20 +0530

    manage dynamic compoenent

--- diamond-ring/src/App.js
+++ diamond-ring/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import RingSelector from './pages/EngagementRing/EngagementRing';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RingSelector from './pages/EngagementRing/EngagementRing';
+import RingDetails from './pages/EngagementRingDetail/EngagementRingDetail'; // Create this component for detailed view
 
 function App() {
   return (
     <Provider store={store}>
-      <RingSelector />
-    </Provider>);
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="ring-select" element={<RingSelector />} />
+          {/* Route with ID */}
+          <Route path="/ring-select/ring-detail-page" element={<RingDetails />} />
+          {/* Redirect to default route */}
+          <Route path="*" element={<RingSelector />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
-
---
+++ diamond-ring/src/pages/EngagementRingDetail/EngagementRingDetail.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import TabComponent from '../../Component/Common/TabComponent'
+
+const EngagementRingDetail = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Header */}
+      <div className="bg-black text-white text-center py-2 text-sm">
+        Order TODAY for Guaranteed Holiday Delivery!
+      </div>
+
+      {/* Navigation */}
+ 
+
+      <TabComponent />
+
+   
+    </div>
+  )
+}
+
+export default EngagementRingDetail
